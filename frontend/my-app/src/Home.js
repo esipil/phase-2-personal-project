a82abbcfed96a739e9f820f7bd5b94b93e57e2e8
Timothy <lemayiant4@gmail.com>
2022-10-04 22:37:06 +0300

    editing the feedback form

--- frontend/my-app/src/Home.js
+++ frontend/my-app/src/Home.js
@@ -1,6 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function Home() {
+
+  const [formData, setFormData] = useState({
+    comment:""
+  });
+  function handleChange (event) {
+    setFormData({
+      ...formData,
+      [event.target.name]:event.target.value
+    });
+  }
+
+  function handleSubmit (event) {
+    event.preventDefault();
+    event.reset()
+    fetch("http://localhost:3000/feedback", {
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body:JSON.stringify({
+        comment:formData.comment
+      })
+    })
+  }
+
   return (
     <div className='home'>
       <div className='homePage'>
@@ -18,9 +43,15 @@ function Home() {
       </div>
       <div className="feedback">
         <h3  className='feedback-notice'>Tell Us How You Dined</h3>
-        <form>
+        <form onClick={handleSubmit}>
           <label className='hhh'> we value your feedback.uykjhvb kjhgncb 
-            <input type={"text"} className="input"></input>
+            <input 
+            type={"text"} 
+            className="input"
+            name="comment"
+            value={formData.comment}
+            onChange={handleChange}
+            ></input>
           </label>
           <div className='button'>
           <button  type='submit'>Submit</button>
