fc19e5295f3b1b927b3136c53cf99171c902318e
Timothy <lemayiant4@gmail.com>
2022-10-04 20:24:50 +0300

    menus added

--- frontend/my-app/src/menu/Menu.js
+++ frontend/my-app/src/menu/Menu.js
@@ -1,38 +1,21 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import MenuPage from './MenuPage'
 
 function Menu() {
-  
+  const [meals, setMeals] = useState([])
+  useEffect( () => {
+    fetch("http://localhost:3000/food")
+    .then(data => data.json())
+    .then(data => setMeals(data))
+  },[])
+  console.log(meals)
+  const renderMeals = meals.map(item => {
+     return < MenuPage key={item.id} meal={item} />
+  })
   return (
-    <div>
-      <div className='daily-menu'>
-        <h2 className='notice'>j</h2>
-        <div className='breakfast'>
-          <div className='breakfast-image'>
-          </div>
-          <div className='breakfast-contents'>
-          </div>
-        </div>
-        <div className='lunch'>
-          <div className='breakfast-contents'>
-          </div>
-          <div className='breakfast-image'>
-          </div>
-        </div>
-        <div className='dinner'>
-          <div className='breakfast-image'>
-          </div>
-          <div className='breakfast-contents'>
-          </div>
-        </div>
-        <div className='beverages'>
-          <div className='breakfast-contents'>
-          </div>
-          <div className='breakfast-image'>
-          </div>
-        </div>
-      </div>
-      <div className='chef-special'>
-      </div>
+    <div className='daily-menu'>
+      <h2 className='notice'>jhgbgfm</h2>
+      {renderMeals}
     </div>
   )
 }
