72291d0f4d634007454f240cebe2318dfca97b31
Timothy <lemayiant4@gmail.com>
2022-10-05 21:48:16 +0300

    added homeabout

--- frontend/my-app/src/Home.js
+++ frontend/my-app/src/Home.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import HomeAbout from './HomeAbout';
 
 function Home() {
 
@@ -36,15 +37,13 @@ function Home() {
           WHERE EVERY FLAVOUR TELLS A STORY
         </h2>
       </div>
-      <div className='homeContent'>
-        <p class="welcome">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Diam vel quam elementum pulvinar etiam non quam lacus. Sit amet consectetur adipiscing elit ut aliquam purus. Urna molestie at elementum eu facilisis sed. Justo laoreet sit amet cursus sit. Faucibus et molestie ac feugiat sed. Tellus mauris a diam</p>
-        <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </h3>
-        <p class="welcome">Enim nunc faucibus a pellentesque sit amet porttitor eget. Eget egestas purus viverra accumsan in nisl. Augue neque gravida in fermentum et sollicitudin ac orci phasellus. Id eu nisl nunc mi ipsum faucibus. Purus gravida quis blandit turpis cursus. Non diam phasellus vestibulum lorem sed risus ultricies tristique. .</p>
-      </div>
+      <>
+      <HomeAbout/>
+      </>
       <div className="feedback">
         <h3  className='feedback-notice'>Tell Us How You Dined</h3>
         <form onClick={handleSubmit}>
-          <label className='hhh'> we value your feedback.uykjhvb kjhgncb 
+          <label className='hhh'> We Value Your Feedback, It Helps Us Serve You Well
             <input 
             type={"text"} 
             className="input"
